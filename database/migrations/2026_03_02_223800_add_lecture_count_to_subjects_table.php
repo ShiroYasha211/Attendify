1fc138c9d3f8e0a5dd5e244871f880eecffd7622
ShiroYasha211 <138801951+ShiroYasha211@users.noreply.github.com>
2026-03-02 23:12:46 +0300

    feat: add lecture_count column to the subjects table

---
+++ database/migrations/2026_03_02_223800_add_lecture_count_to_subjects_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->integer('lecture_count')->default(0)->after('max_absences');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->dropColumn('lecture_count');
+        });
+    }
+};
