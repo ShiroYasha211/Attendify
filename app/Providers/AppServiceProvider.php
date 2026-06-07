68852b290b708d9e54781226f6028df94e947867
ShiroYasha211 <138801951+ShiroYasha211@users.noreply.github.com>
2026-06-07 06:35:23 +0300

    fix devices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+
         \App\Models\Attendance::observe(\App\Observers\AttendanceObserver::class);
         \App\Models\Academic\Assignment::observe(\App\Observers\AssignmentObserver::class);
         \App\Models\Announcement::observe(\App\Observers\AnnouncementObserver::class);
